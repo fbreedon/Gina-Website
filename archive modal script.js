8429501cfd7c72af1f010df522a7f6c737aa09b9
Franz Breedon <fbreedon@ucsc.edu>
2018-10-27 22:04:18 -0700

    Archive Modal + More Press
    Wrote the archive modal script to open the modal when an image is clicked, and click through the images and information captions. It should work the same as the other scripts with the php, once that's done. Styled the modal in the html code, since even though I'll replace it with php, it's a lot easier to rapidly iterate and check the styling. Replaced Duet with a lighter, huge version. Added the catalogue links in a styled grid to the press page, with their own thumbnails. Started adding and styling the review links. Still need to comment the new code.

--- archive modal script.js
+++ archive modal script.js
@@ -1,16 +1,38 @@
-var modal = document.getElementsByClassName("archive-modal");
-
-var img = document.getElementsByClassName("archive-img");
-var modalImg = document.getElementsByClassName("modal-img");
-var captionText = document.getElementById("modal-caption");
-img.onclick = function() {
-	modal.style.display = "block";
-	modalImg.src = this.src;
-	captionText.innerHTML = this.alt;
+function openModal() {
+	document.getElementById('modal').style.display = "block";
 }
 
-var span = document.getElementsByClassName("modal-close")[0];
+function closeModal() {
+	document.getElementById('modal').style.display = "none";
+}
 
-span.onclick = function() {
-	modal.style.display = "none";
+var modalIndex = 1;
+showModal(modalIndex);
+
+function plusModal(n) {
+	showModal(modalIndex += n);
+}
+
+function currentModal(n) {
+	showModal(modalIndex = n);
+}
+
+function showModal(n) {
+	var i;
+	var images = document.getElementsByClassName("modal-img");
+	var captionText = document.getElementsByClassName("info");
+
+	if (n > images.length) {modalIndex = 1;}
+	if (n < 1) {modalIndex = images.length;}
+
+	for (i = 0; i < images.length; i++) {
+		images[i].style.display = "none";
+	}
+
+	for (i = 0; i < captionText.length; i++) {
+		captionText[i].style.display = "none";
+	}
+
+	images[modalIndex-1].style.display = "block";
+	captionText[modalIndex-1].style.display = "block";
 }
--- slideshow script.js
+++ slideshow script.js
@@ -20,5 +20,5 @@ function slideShow() {
 
 	slides[slideIndex-1].style.display = "block";
 	// Change the image every 4 seconds
-	setTimeout(slideShow, 4000);
+	setTimeout(slideShow, 4500);
 }
